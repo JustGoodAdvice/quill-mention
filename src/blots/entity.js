dffc627e5b0397820797ba4ceffd4bee30424e65
Mark Healey <markhealey@gmail.com>
2019-04-18 11:05:20 -0600

    Update entity.js

--- src/blots/entity.js
+++ src/blots/entity.js
@@ -3,15 +3,15 @@ import Quill from 'quill';
 const Inline = Quill.import('blots/inline');
 
 class Entity extends Inline {
-	static create(data) {
-		const node = super.create(data);
-		node.setAttribute('href', '#');
-		node.setAttribute('data-entity-id', data.id);
-		node.setAttribute('data-entity-content', data.content);
-		node.setAttribute('spellcheck', false);
-		node.setAttribute('data-toggle', "popover");
-		return node;
-	}
+  static create(data) {
+    const node = super.create(data);
+    node.setAttribute('href', '#');
+    node.setAttribute('data-entity-id', data.id);
+    node.setAttribute('data-entity-content', data.content);
+    node.setAttribute('spellcheck', false);
+    node.setAttribute('data-toggle', 'popover');
+    return node;
+  }
 }
 
 Entity.blotName = 'entity';
