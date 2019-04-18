87376d7976e512ce4cafcc0c9023055338208fdc
Mark Healey <markhealey@gmail.com>
2019-04-17 23:36:10 -0600

    Customizations for Entity format

--- src/blots/entity.js
+++ src/blots/entity.js
@@ -5,11 +5,11 @@ const Inline = Quill.import('blots/inline');
 class Entity extends Inline {
 	static create(data) {
 		const node = super.create(data);
-		console.log("BLOT", data)
 		node.setAttribute('href', '#');
 		node.setAttribute('data-entity-id', data.id);
 		node.setAttribute('data-entity-content', data.content);
 		node.setAttribute('spellcheck', false);
+		node.setAttribute('data-toggle', "popover");
 		return node;
 	}
 }
